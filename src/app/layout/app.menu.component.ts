57a03ae546a06c9b5d3025cc322f022e0bbff4af
Quentin Legiemble <q.legiemble@gmail.com>
2023-10-23 00:45:10 +0200

    init template + creation employee module/component

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -1,6 +1,7 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { LayoutService } from './service/app.layout.service';
+import {ActivatedRoute, IsActiveMatchOptions, Router} from "@angular/router";
 
 @Component({
     selector: 'app-menu',
@@ -10,156 +11,80 @@ export class AppMenuComponent implements OnInit {
 
     model: any[] = [];
 
-    constructor(public layoutService: LayoutService) { }
+    openedMenuIndex: number | null = null;
+
+    toggleMenu(index: number): void {
+      this.openedMenuIndex = this.openedMenuIndex === index ? null : index;
+    }
+
+  constructor(public layoutService: LayoutService, private router: Router, private activatedRoute: ActivatedRoute) { }
+
+  isMenuItemActive(menu: { items: any[]; }): boolean {
+    const exactMatchOptions: IsActiveMatchOptions = {
+      paths: 'exact',
+      queryParams: 'exact',
+      fragment: 'ignored',
+      matrixParams: 'ignored'
+    };
+    return menu.items.some(item => this.router.isActive(item.routerLink[0], exactMatchOptions));
+  }
 
     ngOnInit() {
         this.model = [
-            {
-                label: 'Home',
-                items: [
-                    { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }
-                ]
-            },
-            {
-                label: 'UI Components',
-                items: [
-                    { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', routerLink: ['/uikit/formlayout'] },
-                    { label: 'Input', icon: 'pi pi-fw pi-check-square', routerLink: ['/uikit/input'] },
-                    { label: 'Float Label', icon: 'pi pi-fw pi-bookmark', routerLink: ['/uikit/floatlabel'] },
-                    { label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
-                    { label: 'Button', icon: 'pi pi-fw pi-box', routerLink: ['/uikit/button'] },
-                    { label: 'Table', icon: 'pi pi-fw pi-table', routerLink: ['/uikit/table'] },
-                    { label: 'List', icon: 'pi pi-fw pi-list', routerLink: ['/uikit/list'] },
-                    { label: 'Tree', icon: 'pi pi-fw pi-share-alt', routerLink: ['/uikit/tree'] },
-                    { label: 'Panel', icon: 'pi pi-fw pi-tablet', routerLink: ['/uikit/panel'] },
-                    { label: 'Overlay', icon: 'pi pi-fw pi-clone', routerLink: ['/uikit/overlay'] },
-                    { label: 'Media', icon: 'pi pi-fw pi-image', routerLink: ['/uikit/media'] },
-                    { label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
-                    { label: 'Message', icon: 'pi pi-fw pi-comment', routerLink: ['/uikit/message'] },
-                    { label: 'File', icon: 'pi pi-fw pi-file', routerLink: ['/uikit/file'] },
-                    { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/uikit/charts'] },
-                    { label: 'Misc', icon: 'pi pi-fw pi-circle', routerLink: ['/uikit/misc'] }
-                ]
-            },
-            {
-                label: 'Prime Blocks',
-                items: [
-                    { label: 'Free Blocks', icon: 'pi pi-fw pi-eye', routerLink: ['/blocks'], badge: 'NEW' },
-                    { label: 'All Blocks', icon: 'pi pi-fw pi-globe', url: ['https://www.primefaces.org/primeblocks-ng'], target: '_blank' },
-                ]
-            },
-            {
-                label: 'Utilities',
-                items: [
-                    { label: 'PrimeIcons', icon: 'pi pi-fw pi-prime', routerLink: ['/utilities/icons'] },
-                    { label: 'PrimeFlex', icon: 'pi pi-fw pi-desktop', url: ['https://www.primefaces.org/primeflex/'], target: '_blank' },
-                ]
-            },
-            {
-                label: 'Pages',
-                icon: 'pi pi-fw pi-briefcase',
-                items: [
-                    {
-                        label: 'Landing',
-                        icon: 'pi pi-fw pi-globe',
-                        routerLink: ['/landing']
-                    },
-                    {
-                        label: 'Auth',
-                        icon: 'pi pi-fw pi-user',
-                        items: [
-                            {
-                                label: 'Login',
-                                icon: 'pi pi-fw pi-sign-in',
-                                routerLink: ['/auth/login']
-                            },
-                            {
-                                label: 'Error',
-                                icon: 'pi pi-fw pi-times-circle',
-                                routerLink: ['/auth/error']
-                            },
-                            {
-                                label: 'Access Denied',
-                                icon: 'pi pi-fw pi-lock',
-                                routerLink: ['/auth/access']
-                            }
-                        ]
-                    },
-                    {
-                        label: 'Crud',
-                        icon: 'pi pi-fw pi-pencil',
-                        routerLink: ['/pages/crud']
-                    },
-                    {
-                        label: 'Timeline',
-                        icon: 'pi pi-fw pi-calendar',
-                        routerLink: ['/pages/timeline']
-                    },
-                    {
-                        label: 'Not Found',
-                        icon: 'pi pi-fw pi-exclamation-circle',
-                        routerLink: ['/notfound']
-                    },
-                    {
-                        label: 'Empty',
-                        icon: 'pi pi-fw pi-circle-off',
-                        routerLink: ['/pages/empty']
-                    },
-                ]
-            },
-            {
-                label: 'Hierarchy',
-                items: [
-                    {
-                        label: 'Submenu 1', icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            {
-                                label: 'Submenu 1.1', icon: 'pi pi-fw pi-bookmark',
-                                items: [
-                                    { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
-                                    { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
-                                    { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' },
-                                ]
-                            },
-                            {
-                                label: 'Submenu 1.2', icon: 'pi pi-fw pi-bookmark',
-                                items: [
-                                    { label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }
-                                ]
-                            },
-                        ]
-                    },
-                    {
-                        label: 'Submenu 2', icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            {
-                                label: 'Submenu 2.1', icon: 'pi pi-fw pi-bookmark',
-                                items: [
-                                    { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
-                                    { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' },
-                                ]
-                            },
-                            {
-                                label: 'Submenu 2.2', icon: 'pi pi-fw pi-bookmark',
-                                items: [
-                                    { label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' },
-                                ]
-                            },
-                        ]
-                    }
-                ]
-            },
-            {
-                label: 'Get Started',
-                items: [
-                    {
-                        label: 'Documentation', icon: 'pi pi-fw pi-question', routerLink: ['/documentation']
-                    },
-                    {
-                        label: 'View Source', icon: 'pi pi-fw pi-search', url: ['https://github.com/primefaces/sakai-ng'], target: '_blank'
-                    }
-                ]
-            }
+          {
+            label: 'Gestion client',
+            items: [
+              { label: 'Clients', icon: 'pi pi-fw pi-users', routerLink: ['/sdfgsdfg'] },
+              { label: 'Retours', icon: 'pi pi-fw pi-users', routerLink: ['/dsgsdfgsdg'] },
+            ]
+          },
+          {
+            label: 'Management',
+            items: [
+                { label: 'Employés', icon: 'pi pi-fw pi-users', routerLink: ['/'] },
+                { label: 'Absences/retards', icon: 'pi pi-fw pi-users', routerLink: ['/fsqffds'] },
+                { label: 'Planning', icon: 'pi pi-fw pi-users', routerLink: ['/fdsfsdfdf'] },
+                { label: 'Feedback', icon: 'pi pi-fw pi-users', routerLink: ['/fsdfsfsd'] },
+            ]
+          },
+          {
+            label: 'Gestion des stocks',
+            items: [
+              { label: 'Stock', icon: 'pi pi-fw pi-users', routerLink: ['/qsfdsqdf'] },
+              { label: 'Commandes en cours', icon: 'pi pi-fw pi-users', routerLink: ['/sqfdsfq'] },
+              { label: 'Historique des commandes', icon: 'pi pi-fw pi-users', routerLink: ['/sdfsdqf'] },
+              { label: 'Produits', icon: 'pi pi-fw pi-users', routerLink: ['/sqdfsfdq'] },
+              { label: 'Pertes', icon: 'pi pi-fw pi-users', routerLink: ['/sqdfsdff'] },
+            ]
+          },
+          {
+            label: 'Marketing',
+            items: [
+              { label: 'Publicités', icon: 'pi pi-fw pi-users', routerLink: ['/sdfsffdf'] },
+              { label: 'Campagnes publicitaires', icon: 'pi pi-fw pi-users', routerLink: ['/sqdfsf'] },
+              { label: 'Promotions', icon: 'pi pi-fw pi-users', routerLink: ['/sqdffsdqf'] },
+              { label: 'Tendances', icon: 'pi pi-fw pi-users', routerLink: ['/qsfsdfqf'] },
+              { label: 'Promotions', icon: 'pi pi-fw pi-users', routerLink: ['/sqffdsqfsd'] },
+            ]
+          },
+          {
+            label: 'Restaurants',
+            items: [
+              { label: 'Restaurants', icon: 'pi pi-fw pi-users', routerLink: ['/sfsdffdq'] },
+            ]
+          },
+          {
+            label: 'Gestion des chiffres',
+            items: [
+              { label: 'Ventes', icon: 'pi pi-fw pi-users', routerLink: ['/sqdsdfsqdff'] },
+            ]
+          },
         ];
+        for (let i = 0; i < this.model.length; i++) {
+          if (this.isMenuItemActive(this.model[i])) {
+            this.openedMenuIndex = i;
+            break;
+          }
+        }
     }
 }
