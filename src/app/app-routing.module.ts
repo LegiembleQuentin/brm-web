51dc80cb0a7181c92163a413aa216e330cd10ba0
ArthurJfr <jaffro.arthur@gmail.com>
2023-12-11 16:29:57 +0100

    rebase work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,19 @@ import {EmployeeDetailComponent} from "./entities/employee/employee-detail/emplo
 import {VerifyTokenComponent} from "./verify-token/verify-token.component";
 
 import {LoginComponent} from "./login/login.component";
+import {FeedbackListingComponent} from "./entities/feedback/feedback-listing/feedback-listing.component";
+import {RestaurantListingComponent} from "./entities/restaurant/restaurant-listing/restaurant-listing.component";
+import {RestaurantDetailComponent} from "./entities/restaurant/restaurant-detail/restaurant-detail.component";
+import {AbsenceListingComponent} from "./entities/absence/absence-listing/absence-listing.component";
+import {StockListingComponent} from "./entities/stock/stock-listing/stock-listing.component";
+import {StockDetailComponent} from "./entities/stock/stock-detail/stock-detail.component";
+import {ProductListingComponent} from "./entities/product/product-listing/product-listing.component";
+import {ProductDetailComponent} from "./entities/product/product-detail/product-detail.component";
+import {NotFoundComponent} from "./not-found/not-found/not-found.component";
 
 
 const routes: Routes = [
+
   {
     path: '', component: AppLayoutComponent,
     children: [
@@ -19,9 +29,47 @@ const routes: Routes = [
           { path: ':id', component: EmployeeDetailComponent }
         ]
       },
-
+      {
+        path: 'feedback',
+        children: [
+          { path: '', component: FeedbackListingComponent },
+        ]
+      },
+      {
+        path: 'restaurant',
+        children: [
+          { path: '', component: RestaurantListingComponent },
+          { path: ':id', component: RestaurantDetailComponent }
+        ]
+      },
+      {
+        path: 'absence',
+        children: [
+          { path: '', component: AbsenceListingComponent },
+        ]
+      },
+      {
+        path: 'stock',
+        children: [
+          { path: '', component: StockListingComponent },
+          { path: ':id', component: StockDetailComponent }
+        ]
+      },
+      {
+        path: 'product',
+        children: [
+          { path: '', component: ProductListingComponent },
+          { path: ':id', component: ProductDetailComponent },
+        ]
+      },
       { path: 'verify-token', component: VerifyTokenComponent },
-      { path: 'login', component: LoginComponent }
+      { path: 'login', component: LoginComponent },
+
+
+
+
+      { path: '404', component: NotFoundComponent },
+      { path: '**', redirectTo: '/404' }
     ]
   }
 ];
